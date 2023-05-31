e161f26ae35bf51725d8a24169219bf360423d5e
JulienNirxo <millaud.julien@gmail.com>
2023-05-31 10:43:42 +0200

    [FIX] indentation

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,16 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-  if ('serviceWorker' in navigator) {
-    console.log("Service Worker is supported");
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/assets/serviceworker.js')
-            .then((reg) => {
-                console.log('Service worker registered.', reg);
-            })
-            .catch((err) => {
-                console.log('Service worker registration failed:', err);
-            }
-            );
-    });
+if ('serviceWorker' in navigator) {
+  console.log("Service Worker is supported");
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/assets/serviceworker.js')
+      .then((reg) => {
+        console.log('Service worker registered.', reg);
+      })
+      .catch((err) => {
+        console.log('Service worker registration failed:', err);
+      }
+      );
+  });
 }
