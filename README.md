e1243d3f51ee66aa2a9689a4af892cd5089e73e5
JulienNirxo <millaud.julien@gmail.com>
2023-05-31 15:39:41 +0200

    [FEAT] explication for build on your phone

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Pwa
 
 For run this project, use command "npm run startPWA" and go to url "http://127.0.0.1:8080/"
+
+#Run on mobile phone
+
+-> Your phone and your computer on the same wifi
+-> Go to web application on your phone and get computer ip
+-> Enjoy !
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core'
 import { SwPush } from '@angular/service-worker'
 
-const VAPID_PUBLIC =
-  'BPIf3btGAP3Sq2f-4S0WS2DtbCSnbFbtYGA_6c6nNZ_DsIaCLMFu1fxO30Odkcy8ror-5OdP4_4vyJO4FTZVGYY'
+const VAPID_PUBLIC = 'BPIf3btGAP3Sq2f-4S0WS2DtbCSnbFbtYGA_6c6nNZ_DsIaCLMFu1fxO30Odkcy8ror-5OdP4_4vyJO4FTZVGYY'
 
 @Component({
   selector: 'app-root',
