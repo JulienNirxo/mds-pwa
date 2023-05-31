74a0be6cb21c80a8a838baac1a040424658cf374
JulienNirxo <millaud.julien@gmail.com>
2023-05-31 10:40:27 +0200

    [FIX] pwa service worker

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,17 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  if ('serviceWorker' in navigator) {
+    console.log("Service Worker is supported");
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/assets/serviceworker.js')
+            .then((reg) => {
+                console.log('Service worker registered.', reg);
+            })
+            .catch((err) => {
+                console.log('Service worker registration failed:', err);
+            }
+            );
+    });
+}
