7b4a80432dda4af31703d4395c056a4c28749435
JulienNirxo <millaud.julien@gmail.com>
2023-06-21 13:46:50 +0200

    [FEAT] Pusher NOT WORKING

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core'
 import { SwPush } from '@angular/service-worker'
+import Pusher from 'pusher-js';
 
-const VAPID_PUBLIC = 'BPIf3btGAP3Sq2f-4S0WS2DtbCSnbFbtYGA_6c6nNZ_DsIaCLMFu1fxO30Odkcy8ror-5OdP4_4vyJO4FTZVGYY'
+
+const VAPID_PUBLIC = 'BPIf3btGAP3Sq2f-4S0WS2DtbCSnbFbtYGA_6c6nNZ_DsIaCLMFu1fxO30Odkcy8ror-5OdP4_4vyJO4FTZVGYY';
 
 @Component({
   selector: 'app-root',
@@ -9,32 +11,38 @@ const VAPID_PUBLIC = 'BPIf3btGAP3Sq2f-4S0WS2DtbCSnbFbtYGA_6c6nNZ_DsIaCLMFu1fxO30
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular'
+  title = 'angular';
 
-  //création d'un constructeur pour l'initialisation du service worker
-  constructor(swPush: SwPush) {
+  constructor(private swPush: SwPush) {
     if (swPush.isEnabled) {
       swPush
         .requestSubscription({
           serverPublicKey: VAPID_PUBLIC,
         })
         .then(subscription => {
-          console.log(subscription)
-          this.showPushNotification()
+          console.log(subscription);
+          this.showPushNotification();
         })
-        .catch(console.error)
+        .catch(console.error);
     }
   }
 
-  //fonction pour afficher la notification
   showPushNotification() {
-    if (window.Notification && Notification.permission !== "denied") {
+    if (window.Notification && Notification.permission !== 'denied') {
       Notification.requestPermission(function (status) {
-        var n = new Notification("Ma notification", {
-          body: "Salut MDS !",
+        const n = new Notification('Ma notification', {
+          body: 'Salut MDS!',
         });
       });
     }
 
+    const pusher = new Pusher('67377cc9-cb2e-499e-b07b-b637506cf3b8', {
+      cluster: 'ECF800C959B3B96F2FB99C4E97CACF304399F7D69A82DA8A72ADD197718CF303',
+    });
+
+    const channel = pusher.subscribe('nom-du-canal');
+    channel.bind('nom-de-l-événement', (data: any) => {
+      console.log('Notification reçue:', data);
+    });
   }
 }
